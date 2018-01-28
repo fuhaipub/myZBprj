8738af6575293da9c8e05f7b8122de0168a4ab28
fuhaipub <fuhaipub@qq.com>
2018-01-29 00:25:37 +0800

    实现了workflow的整体框架,增加了一个topsell(高位卖出)的样例

--- src/app/topSell.py
+++ src/app/topSell.py
@@ -27,7 +27,7 @@ class TopSellPattern(Pattern):
         super(TopSellPattern, self).__init__()
         pass
 
-    def matching(self, event):
+    def matching(self, event):  #通过重载一个matching来判断是否符合模式;如果符合模式,讲想要输出的结果放到result中,传递给Policy和Action,并返回True
         #todo 匹配上涨的顶峰区间
         print "TopSellPattern: matching 匹配上涨的顶峰区间"
         self.result={"action":"buy"}
@@ -38,7 +38,7 @@ class TopSellAction(Action):
     def __init__(self):
         super(TopSellAction, self).__init__()
 
-    def do(self, newEvent):
+    def do(self, newEvent):  #实现真正要做的事情
         print "TopSellAction: newEvent="+ str(newEvent)
 
 
@@ -46,7 +46,7 @@ class TopSellPolicy(Policy):
     def __init__(self):
         super(TopSellPolicy, self).__init__()
 
-    def rule(self, newEvent):
+    def rule(self, newEvent):  # 增加规律判断代码,如果通过,返回True, 不满足返回False
         print "TopSellPolicy: newEvent="+ str(newEvent)
         return True
 
